cc37dffb794a944a43e203e7f7cb0e2944098837
lialinhares <lialinhares27@gmail.com>
2024-09-24 00:26:32 -0300

    Alterando frontend

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './Home.css'
+import {useNavigate} from "react-router-dom";
 
 const Home = () => {
+
     return (
-        <div>
-            <h1>Bem-vindo ao Sistema de Vendas</h1>
-            <p>Use o menu para navegar pelas funcionalidades.</p>
+        <div className=''>
+            <div className='mensagem'>
+                <h1>Bem-vindo à Biblioteca Online</h1>
+                <p>Use o menu para navegar pelas funcionalidades.</p>
+            </div>
         </div>
     );
 };
